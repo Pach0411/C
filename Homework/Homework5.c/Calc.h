3383f13b492b98cc8c76e4aef63a2234291ebbf9
Pach0411 <78566505+Pach0411@users.noreply.github.com>
2021-04-22 12:10:10 -0500

    Create Calc.h

---
+++ Homework/Homework5.c/Calc.h
@@ -0,0 +1,15 @@
+#define forloop(i, x) for(i = 0; i != x; i++)
+#define BOMB -1  //set bombs//
+#define CONTL 10  //number of rows and columns (counter line)//
+
+#define LOST 'l' 
+#define WON 'O'
+#define OK '?'
+
+void bombs(int);
+void neighbours(int); 
+int open();
+void kaboom();
+void empty(int, int);
+char field(int, int);
+void set(int, int, char);
