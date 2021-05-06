9592210b9942bd0d795b06ee46a7df010a2a8d7b
Pach0411 <78566505+Pach0411@users.noreply.github.com>
2021-05-06 15:00:28 -0500

    main.c

--- Homework/homework7.c/main.c
+++ Homework/homework7.c/main.c
@@ -122,3 +122,5 @@ int main(){
 
 /*Este programa fue creado con ayuda del repo de la profe que es https://github.com/satuelisa/C/tree/main/Ch6, Brian W. Kernighan, Dennis M. Ritchie - The C Programming Language, Second Edition (1988) y tambien
 con ayuda de videos en youtube*/
+
+
