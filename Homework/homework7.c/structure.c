b97058306935caccfe1c677b8827c803b5e6f026
Pach0411 <78566505+Pach0411@users.noreply.github.com>
2021-05-06 12:44:02 -0500

    structure.c

--- Homework/homework7.c/structure.c
+++ Homework/homework7.c/structure.c
@@ -1,3 +1,10 @@
+/* This code work perfectly in https://www.onlinegdb.com, first you have to paste all the codes in the gdb with the name of the codes that in this case is "main.c, structure.c, instructions.h" 
+   you will paste the code regarding their name*/
+/* Este codigo funciona en https://www.onlinegdb.com o en el codificador de tu preferencia, solo lo que debes de hacer es pegar los 3 archivos que estan en mi carpeta de esta tarea
+   y al igual debes de ponerlos con el nombre con el cual los encontraras que son "main.c, structure.c, instructions.h" y ya:) */
+
+/* CONTRASEÑA DE ADMIN ES 007 */
+
 #include "instructions.h"
 
 int crearRegistro(void){
